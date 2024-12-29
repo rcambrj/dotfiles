73b4f0101106cada703f908a2627e716ca5cd0e6
Robert Cambridge <robert@cambridge.me>
2024-12-29 19:06:38 +0100

    simplify mango config for installation

--- hosts/mango/configuration.nix
+++ hosts/mango/configuration.nix
@@ -1,23 +1,16 @@
 # ThinkPad T14S workstation laptop
 { flake, inputs, perSystem, ... }: {
   imports = [
-    inputs.home-manager.darwinModules.home-manager
     flake.nixosModules.base
-    flake.nixosModules.common
+    # flake.nixosModules.common
     flake.nixosModules.access-server # TODO: remove this
     flake.nixosModules.access-workstation
     flake.nixosModules.standard-disk
-    ./graphical.nix
+    # ./graphical.nix
+    # ./home.nix
   ];
 
   networking.hostName = "mango";
   nixpkgs.hostPlatform = "x86_64-linux";
 
-  home-manager.users.rcambrj.imports = [
-    flake.homeModules.rcambrj
-    { home.stateVersion = "23.11"; }
-  ];
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = { inherit inputs perSystem; };
 }
---
+++ hosts/mango/home.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  imports = [
+    inputs.home-manager.darwinModules.home-manager
+  ];
+
+  home-manager.users.rcambrj.imports = [
+    flake.homeModules.rcambrj
+    { home.stateVersion = "23.11"; }
+  ];
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.extraSpecialArgs = { inherit inputs perSystem; };
+
+}
