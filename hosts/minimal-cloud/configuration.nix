654c9466397f1f4d9b7f4efd0c3b2117b25da6d4
Robert Cambridge <robert@cambridge.me>
2025-01-14 18:48:45 +0100

    add ssh access config to minimal-cloud

--- hosts/minimal-cloud/configuration.nix
+++ hosts/minimal-cloud/configuration.nix
@@ -3,15 +3,16 @@
     "${toString modulesPath}/profiles/qemu-guest.nix"
 
     flake.nixosModules.base
+    flake.nixosModules.access-server
     flake.nixosModules.standard-disk
     flake.nixosModules.root-keys
     inputs.disko.nixosModules.disko
   ];
   networking.hostName = "minimal-cloud";
 
-  nixpkgs.hostPlatform = "x86_64-linux";
-  # nixpkgs.hostPlatform = "aarch64-linux";
+  # nixpkgs.hostPlatform = "x86_64-linux";
+  nixpkgs.hostPlatform = "aarch64-linux";
 
-  # disko.devices.disk.disk1.device = "/dev/sda";
+  disko.devices.disk.disk1.device = "/dev/sda";
   # disko.devices.disk.disk1.device = "/dev/vda";
 }
