bed14729e45867dcdb5dcbb305c268c1095b9dd9
Robert Cambridge <robert@cambridge.me>
2025-03-30 12:04:11 +0000

    get the camera working gud

--- hosts/gooseberry/ustreamer.nix
+++ hosts/gooseberry/ustreamer.nix
@@ -1,28 +1,42 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  port = 22678;
+in {
   # v4l2-ctl --list-devices
   # v4l2-ctl --dev /dev/video0 --list-formats-ext
   environment.systemPackages = with pkgs; [
     v4l-utils
   ];
 
+  networking.firewall.allowedTCPPorts = [
+    port
+  ];
+
   services.ustreamer = {
     enable = true;
-    # device = "/dev/v4l/by-id/usb-webcamvendor_webcamproduct_00000000-video-index0";
+    listenAddress = "0.0.0.0:${builtins.toString port}";
+    # https://github.com/pikvm/ustreamer/blob/afd305e87dd2927d8e65da77f5ccfd0c7c119bd3/man/ustreamer.1
+    # https://github.com/mainsail-crew/crowsnest/blob/d0c2ca5d1613d81cdb17cded1438d4bc5d6b0995/libs/ustreamer.sh#L59
     extraArgs = [
       "--resolution=2560x1440"
-      "-f 30"
-      # https://github.com/mainsail-crew/crowsnest/blob/d0c2ca5d1613d81cdb17cded1438d4bc5d6b0995/libs/ustreamer.sh#L59
-      # "-m MJPEG"
-      # "--encoder=HW"
+      "--desired-fps=15"
+      "--device-timeout=2"
+      "--format=MJPEG"
+
+      # hardware offloading
+      # "--encoder=M2M-VIDEO" # GPU-accelerated MJPEG encoding
+      "--encoder=HW" # Use pre-encoded MJPEG frames directly from camera hardware
+
+      # "--quality=80" # not supported by this camera
     ];
   };
 
   services.fluidd.nginx.locations = {
     "/webcam/snapshot" = {
-      proxyPass = "http://localhost:8080/snapshot";
+      proxyPass = "http://localhost:${builtins.toString port}/snapshot";
     };
-    "/webcam/stream" = {
-      proxyPass = "http://localhost:8080/stream";
-    };
+    # this is very CPU-intensive on nginx, probably due to HTTPS encryption
+    # "/webcam/stream" = {
+    #   proxyPass = "http://localhost:${builtins.toString port}/stream";
+    # };
   };
 }
