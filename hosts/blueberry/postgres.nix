d21e24cb176beb9f1bf8c07ff591aa826d420a54
Robert Cambridge <robert@cambridge.me>
2025-03-31 15:54:54 +0200

    dont bring up pgadmin because its seldom needed and fails frequently

--- hosts/blueberry/postgres.nix
+++ hosts/blueberry/postgres.nix
@@ -3,14 +3,15 @@
     enable = true;
   };
 
-  services.pgadmin = {
-    enable = true;
-    initialEmail = "robert@cambridge.me";
-    initialPasswordFile = config.age.secrets.blueberry-pgadmin.path;
-    settings = {
-      CONFIG_DATABASE_URI = "postgresql://%2Frun%2Fpostgresql";
-    };
-  };
+  # pgadmin regularly fails to come up. will investigate why when I can be bothered
+  # services.pgadmin = {
+  #   enable = true;
+  #   initialEmail = "robert@cambridge.me";
+  #   initialPasswordFile = config.age.secrets.blueberry-pgadmin.path;
+  #   settings = {
+  #     CONFIG_DATABASE_URI = "postgresql://%2Frun%2Fpostgresql";
+  #   };
+  # };
 
   services.postgresql.ensureUsers = [{
     name = "pgadmin";
