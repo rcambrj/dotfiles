ff0fc8c56c43d18a23c78bc45150b00eef1dcd68
Robert Cambridge <robert@cambridge.me>
2025-04-13 17:27:43 +0200

    optimise server machines

--- modules/nixos/cloud-vps.nix
+++ modules/nixos/cloud-vps.nix
@@ -4,4 +4,7 @@
   ];
 
   age.identityPaths = [ "/root/.ssh/id_ed25519" ];
+
+  boot.loader.grub.configurationLimit = lib.mkDefault 1;
+  boot.loader.systemd-boot.configurationLimit = lib.mkDefault 1;
 }
