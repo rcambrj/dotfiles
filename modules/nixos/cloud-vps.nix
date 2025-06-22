93808e8580cc88ca9673cafa82c5b8e7caec4d0e
Robert Cambridge <robert@cambridge.me>
2025-06-22 11:46:18 +0200

    fix cloud vps eval

--- modules/nixos/cloud-vps.nix
+++ modules/nixos/cloud-vps.nix
@@ -1,10 +1,10 @@
-{ inputs, ... }: _: {
+{ inputs, lib, ... }: {
   imports = [
     inputs.agenix.nixosModules.default
   ];
 
   age.identityPaths = [ "/root/.ssh/id_ed25519" ];
 
-  boot.loader.grub.configurationLimit = lib.mkDefault 1;
+  boot.loader.grub.configurationLimit = 1;
   boot.loader.systemd-boot.configurationLimit = lib.mkDefault 1;
 }
