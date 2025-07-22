49a16c2d6fc68ccc0a575c2b19e87ba0fba471aa
Robert Cambridge <robert@cambridge.me>
2025-07-22 23:19:29 +0200

    bypass cve warning for intel gpu sdk

--- modules/nixos/gpu-intel.nix
+++ modules/nixos/gpu-intel.nix
@@ -12,23 +12,20 @@
   };
   environment.systemPackages = with pkgs; [
     intel-gpu-tools # intel_gpu_top
-
-    # intel-media-sdk
-    # error: Package ‘intel-media-sdk-23.2.2’ in /nix/store/182c2qj20sfspzxcshd6p051qdx3mx5j-source/pkgs/by-name/in/intel-media-sdk/package.nix:65 is marked as insecure, refusing to evaluate.
-    # Known issues:
-    # - End of life with various local privilege escalation vulnerabilites:
-    # - CVE-2023-22656
-    # - CVE-2023-45221
-    # - CVE-2023-47169
-    # - CVE-2023-47282
-    # - CVE-2023-48368
-    # nixpkgs.config.permittedInsecurePackages = [
-    # "intel-media-sdk-23.2.2"
-    # ];
-
-
+    intel-media-sdk
     intel-compute-runtime
     libva-utils
   ];
 
+  # error: Package ‘intel-media-sdk-23.2.2’ is marked as insecure, refusing to evaluate.
+  # Known issues:
+  # - End of life with various local privilege escalation vulnerabilites:
+  # - CVE-2023-22656
+  # - CVE-2023-45221
+  # - CVE-2023-47169
+  # - CVE-2023-47282
+  # - CVE-2023-48368
+  nixpkgs.config.permittedInsecurePackages = [
+    "intel-media-sdk-23.2.2"
+  ];
 }
