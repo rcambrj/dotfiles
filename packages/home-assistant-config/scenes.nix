de9402d80f70a9412031095ddb79bf2d53958359
Robert Cambridge <robert@cambridge.me>
2025-08-04 21:17:23 +0200

    adjust scenes

--- packages/home-assistant-config/scenes.nix
+++ packages/home-assistant-config/scenes.nix
@@ -11,6 +11,20 @@
           state = "open";
           current_position = 8;
         };
+        # rest off
+        "light.hallway_0_light" = "off";
+        "light.hallway_1_light" = "off";
+        "light.hallway_2_light" = "off";
+        "light.living_room_west_light" = "off";
+        "light.living_room_east_light" = "off";
+        "light.living_room_west_lamp" = "off";
+        "switch.living_room_east_lamp" = "off";
+        "light.kitchen_light" = "off";
+        "light.bathroom_light" = "off";
+        "light.bathroom_mirror_light" = "off";
+        "light.office_light" = "off";
+        # "light.guest_room_light" = "off";
+        "light.attic_light" = "off";
       };
     }
     {
@@ -36,16 +50,6 @@
         "light.office_light" = "off";
         # "light.guest_room_light" = "off";
         "light.attic_light" = "off";
-      };
-    }
-    {
-      name = "movie";
-      entities = {
-        "light.living_room_west_light" = "off";
-        "light.living_room_east_light" = "off";
-        "light.living_room_west_lamp" = "on";
-        "switch.living_room_east_lamp" = "on";
-        "light.kitchen_light" = "off";
       };
     }
   ];
