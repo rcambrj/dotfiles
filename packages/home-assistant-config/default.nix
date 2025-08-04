40bc30098bbc1b91143bcc26456d8dfbc9b1a649
Robert Cambridge <robert@cambridge.me>
2025-08-04 10:52:57 +0200

    fix secrets in home-assistant yaml

--- packages/home-assistant-config/default.nix
+++ packages/home-assistant-config/default.nix
@@ -17,12 +17,29 @@
     };
     in eval.config
   );
+
+  # https://github.com/NixOS/nixpkgs/blob/5a0711127cd8b916c3d3128f473388c8c79df0da/nixos/modules/services/home-automation/home-assistant.nix#L59
+  # Post-process YAML output to add support for YAML functions, like
+  # secrets or includes, by naively unquoting strings with leading bangs
+  # and at least one space-separated parameter.
+  # https://www.home-assistant.io/docs/configuration/secrets/
+  format = pkgs.formats.yaml { };
+  renderYAMLFile =
+    fn: yaml:
+    pkgs.runCommand fn
+      {
+        preferLocalBuilds = true;
+      }
+      ''
+        cp ${format.generate fn yaml} $out
+        sed -i -e "s/'\!\([a-z_]\+\) \(.*\)'/\!\1 \2/;s/^\!\!/\!/;" $out
+      '';
 in {
   name = "home-assistant-config";
   dontUnpack = true;
   passthru = configs;
   installPhase = let
-    yamls = mapAttrs (name: value: (pkgs.formats.yaml { }).generate name value) configs;
+    yamls = mapAttrs (name: value: renderYAMLFile name value) configs;
   in ''
     mkdir -p $out
   ''
