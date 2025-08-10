f8cc32b38b72ed6a869c31735931fcd6bb507c2f
Robert Cambridge <robert@cambridge.me>
2025-08-10 20:35:50 +0200

    temporarily disable alloy

--- modules/nixos/telemetry.nix
+++ modules/nixos/telemetry.nix
@@ -15,53 +15,54 @@
   #   };
   # };
 
-  services.alloy = {
-    enable = true;
-    extraFlags = [ "--disable-reporting" ];
-  };
+  # allow seems to block shutdown. unsure why. disable for now.
+  # services.alloy = {
+  #   enable = true;
+  #   extraFlags = [ "--disable-reporting" ];
+  # };
 
-  environment.etc."alloy/config.alloy" = {
-    text = ''
-      loki.write "local" {
-        endpoint {
-          url = "https://loki.home.cambridge.me/loki/api/v1/push"
-        }
-      }
+  # environment.etc."alloy/config.alloy" = {
+  #   text = ''
+  #     loki.write "local" {
+  #       endpoint {
+  #         url = "https://loki.home.cambridge.me/loki/api/v1/push"
+  #       }
+  #     }
 
-      loki.relabel "journal" {
-        forward_to = []
+  #     loki.relabel "journal" {
+  #       forward_to = []
 
-        rule {
-          source_labels = ["__journal__systemd_unit"]
-          target_label  = "unit"
-        }
-        rule {
-          source_labels = ["__journal__boot_id"]
-          target_label  = "boot_id"
-        }
-        rule {
-          source_labels = ["__journal__transport"]
-          target_label  = "transport"
-        }
-        rule {
-          source_labels = ["__journal_priority_keyword"]
-          target_label  = "level"
-        }
-        rule {
-          source_labels = ["__journal__hostname"]
-          target_label  = "instance"
-        }
-      }
+  #       rule {
+  #         source_labels = ["__journal__systemd_unit"]
+  #         target_label  = "unit"
+  #       }
+  #       rule {
+  #         source_labels = ["__journal__boot_id"]
+  #         target_label  = "boot_id"
+  #       }
+  #       rule {
+  #         source_labels = ["__journal__transport"]
+  #         target_label  = "transport"
+  #       }
+  #       rule {
+  #         source_labels = ["__journal_priority_keyword"]
+  #         target_label  = "level"
+  #       }
+  #       rule {
+  #         source_labels = ["__journal__hostname"]
+  #         target_label  = "instance"
+  #       }
+  #     }
 
-      loki.source.journal "read" {
-        forward_to = [
-          loki.write.local.receiver,
-        ]
-        relabel_rules = loki.relabel.journal.rules
-        labels = {
-          "host" = "${config.networking.hostName}",
-        }
-      }
-    '';
-  };
+  #     loki.source.journal "read" {
+  #       forward_to = [
+  #         loki.write.local.receiver,
+  #       ]
+  #       relabel_rules = loki.relabel.journal.rules
+  #       labels = {
+  #         "host" = "${config.networking.hostName}",
+  #       }
+  #     }
+  #   '';
+  # };
 }
