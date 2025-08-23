331c9ad8c44ad0e6acb611592abc75c2856e6fcc
Robert Cambridge <robert@cambridge.me>
2025-08-23 15:49:20 +0200

    some small steps towards cloudberry

--- hosts/cloudberry/configuration.nix
+++ hosts/cloudberry/configuration.nix
@@ -1,27 +1,23 @@
 #
-# this machine is the Internet access
-# * bridging, vlans, routing
-# * vpn
-# * unifi-controller
+# this machine is the Internet router
+# it does bridging, vlans, routing, failover, vpn
 #
 { flake, inputs, ... }: {
   imports = [
+    inputs.nixos-facter-modules.nixosModules.facter
+    { config.facter.reportPath = ./facter.json; }
     inputs.agenix-template.nixosModules.default
-    # inputs.nixos-facter-modules.nixosModules.facter
-    # { config.facter.reportPath = ./facter.json; }
-    flake.nixosModules.base
+
     flake.nixosModules.access-server
-    flake.nixosModules.common
     flake.nixosModules.bare-metal
+    flake.nixosModules.base
+    flake.nixosModules.common
     flake.nixosModules.config-intel
+
     ./interfaces.nix
   ];
 
   networking.hostName = "cloudberry";
-
-  age.secrets = {
-    # todo
-  };
 
   fileSystems = {
     "/var/lib" = {
@@ -35,12 +31,23 @@
     enable = true;
     settings = {
       charger = {
-        governor = "powersave";
-        energy_performance_preference = "power";
-        turbo = "never";
-        # Intel(R) Celeron(R) N5105 @ 2.00GHz
-        # https://askubuntu.com/a/1064309/1682130
+        # Dell Wyse 3040
+        # Intel(R) Atom(TM) x5-Z8350  CPU @ 1.44GHz
+
+        # powersave / balanced / performance
+        governor = "performance";
+
+        # never / auto / always
+        turbo = "auto";
       };
     };
   };
+
+  # Dell Wyse 3040 doesn't have much RAM, but it does have 8GB internal MMC
+  swapDevices = [
+    {
+      device = "/dev/mmcblk0p1";
+    }
+  ];
+  zramSwap.enable = true;
 }
