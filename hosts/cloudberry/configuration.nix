b9233560e573748c3d2f789e108d893ab185c56f
Robert Cambridge <robert@cambridge.me>
2025-09-14 10:20:36 +0200

    disable ipv6 to concentrate on other things

--- hosts/cloudberry/configuration.nix
+++ hosts/cloudberry/configuration.nix
@@ -64,6 +64,7 @@
   zramSwap.enable = true;
 
   boot.kernel.sysctl = {
+    "net.ipv6.conf.all.disable_ipv6" = true; # TODO: enable ipv6
     "net.ipv4.conf.all.forwarding" = true;
     "net.ipv6.conf.all.forwarding" = true;
   };
