de3a7c3bbd2733762a0b8424f4220874d49dab51
Robert Cambridge <robert@cambridge.me>
2026-04-12 01:32:17 +0200

    add platformio

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -91,6 +91,7 @@
         # vscode-marketplace.orsenkucher.vscode-graphql
         vscode-marketplace.tamasfe.even-better-toml
         vscode-marketplace.pinage404.rust-extension-pack
+        vscode-marketplace.platformio.platformio-ide
 
         # use the nixpkgs version because https://github.com/nix-community/nix-vscode-extensions/blob/674d9cb4cfe1a4a989921221b4a2d0e0a4e898a9/nix/removed.nix#L6
         pkgs.vscode-extensions.ms-vscode.cpptools
